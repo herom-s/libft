1fa02fb0616613bf3d6220026d41db5f216f30c3
herom-s <heromapp1@gmail.com>
2025-07-22 12:15:44 -0300

    Adding ft_isdigit working and tested

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,19 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hermarti <hermarti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/21 09:48:04 by hermarti          #+#    #+#             */
-/*   Updated: 2025/07/22 11:55:39 by hermarti         ###   ########.fr       */
+/*   Created: 2025/07/22 11:42:14 by hermarti          #+#    #+#             */
+/*   Updated: 2025/07/22 11:42:35 by hermarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-
-#endif
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
